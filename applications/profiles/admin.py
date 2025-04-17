47740a73b8c59889859e6e616b5c95a8cdb33fe5
compraya2025 <2025compraya@gmail.com>
2025-04-17 10:22:43 -0300

    decima segunda version

--- applications/profiles/admin.py
+++ applications/profiles/admin.py
@@ -4,10 +4,7 @@ from .models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id','photo_profile','full_name','age_profile','neighborhood','ruc_profile',
-                'birth_date',
-                'address'
-            )
+    list_display = ('id','photo_profile','full_name','age_profile','neighborhood','ruc_profile','phone_profile','birth_date','address')
     
     search_fields = ('id','full_name')
 
--- applications/profiles/views.py
+++ applications/profiles/views.py
@@ -96,3 +96,26 @@ class ProfilesViewSet(viewsets.ModelViewSet):
         perfil['city']       = city
 
         return Response(perfil, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        if request.method == 'PUT':
+            return Response({'error': 'Método PUT no permitido. Usa PATCH.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+         
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def partial_update(self, request, pk=None, **kwargs):        
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({"message": "Registro actualizado correctamente."}, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'Registro eliminado existoso!'},status=status.HTTP_204_NO_CONTENT)
